5a2a755e9e7f21de598e992553b2effcdd6f38cc
hashirshoaeb <hashirshoaeb@gmail.com>
2021-12-12 15:28:46 +0500

    performance optimization and null error fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,109 +18,110 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _loading = false;
+  bool _loading = true;
   File? _image;
   List<dynamic>? _outputs;
   final _imagePicker = ImagePicker();
 
+  @override
   void initState() {
     super.initState();
-    _loading = true;
-    loadModel().then((value) {
-      setState(() {
-        _loading = false;
-      });
-    });
+    loadModel();
   }
 
-  loadModel() async {
-    await Tflite.loadModel(
+  Future<void> loadModel() async {
+    final res = await Tflite.loadModel(
       model: "assets/model_unquant.tflite",
       labels: "assets/labels.txt",
     );
+    // waits for the model to load before setting the loading state to false
+    setState(() {
+      _loading = false;
+    });
+    print('loaded res: $res');
   }
 
-  pickImage() async {
-    var image = await _imagePicker.getImage(source: ImageSource.gallery);
-    if (image == null) return null;
+  Future<void> pickImage() async {
+    var image = await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
     setState(() {
       _loading = true;
       _image = File(image.path);
     });
-    classifyImage(_image!);
+    await classifyImage(_image!);
   }
 
-  classifyImage(File image) async {
-    var output = await Tflite.runModelOnImage(
+  Future<void> classifyImage(File image) async {
+    final output = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 5,
       threshold: 0.5,
       imageStd: 127.5,
       imageMean: 127.5,
     );
+    print('output: $output');
     setState(() {
       _loading = false;
-      _outputs = output!;
+      _outputs = output;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('OsteoHelp'),
+        title: const Text('OsteoHelp'),
       ),
       body: _loading
-          ? Container(
-              alignment: Alignment.center,
+          ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Container(
+          : SizedBox(
               width: MediaQuery.of(context).size.width,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _image == null
-                      ? Container()
-                      : Container(
+                      ? const SizedBox()
+                      : SizedBox(
                           child: Image.file(_image!),
                           height: 500,
                           width: MediaQuery.of(context).size.width - 200,
                         ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                   _outputs != null
                       ? Text(
-                          "${_outputs![0]["labels"]}"
-                              .replaceAll(RegExp(r'[0-9]'), ''),
+                          "${_outputs![0]["label"]}",
                           style: TextStyle(
                               color: Colors.black,
                               fontSize: 20.0,
                               background: Paint()..color = Colors.white,
                               fontWeight: FontWeight.bold),
                         )
-                      : Text("Classification Waiting")
+                      : const Text("Classification Waiting")
                 ],
               ),
             ),
       floatingActionButton: FloatingActionButton(
         onPressed: pickImage,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
